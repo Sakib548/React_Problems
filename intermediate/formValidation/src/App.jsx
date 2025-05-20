1d2d3e6045e059ef92ec2a24f00fd46570029b39
Md Sakib Hassan <mdsakibhassan548@gmail.com>
2025-05-20 11:48:59 +0600

    formvalidation

--- intermediate/formValidation/src/App.jsx
+++ intermediate/formValidation/src/App.jsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultData = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const [formData, setFormData] = useState(defaultData);
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const validation = () => {
+    const newError = {};
+    if (!formData.name) newError.name = "Name is required";
+    if (!formData.email) newError.email = "Email is required";
+    if (!formData.password) newError.password = "Password is required";
+    if (!formData.email.includes("@")) {
+      newError.email = "Please enter a valid email address";
+    }
+    if (formData.password.length < 6) {
+      newError.password = "Please enter a password of 6 or more characters";
+    }
+    if (formData.password !== formData.confirmPassword) {
+      newError.confirmPassword = "Please enter the same password";
+    }
+
+    return newError;
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validate = validation();
+    if (Object.keys(validate).length === 0) {
+      setFormData(defaultData);
+      setErrors({});
+    } else {
+      setErrors(validate);
+    }
+    console.log(errors);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <br />
+        {errors.name ? <p className="error">{errors.name}</p> : ""}
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <br />
+        {errors.email ? <p className="error">{errors.email}</p> : ""}
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+        <br />
+        {errors.password ? <p className="error">{errors.password}</p> : ""}
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          value={formData.confirmPassword}
+          onChange={handleChange}
+        />
+        <br />
+        {errors.confirmPassword ? (
+          <p className="error">{errors.confirmPassword}</p>
+        ) : (
+          ""
+        )}
+        <button type="submit">Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
